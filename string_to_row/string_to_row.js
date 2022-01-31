f9f15c13708b33b39b55aec5d0116a5e8e586fd0
Игорь <1730020@gmail.com>
2022-02-01 00:11:17 +0300

    the last refactoring

--- string_to_row/string_to_row.js
+++ string_to_row/string_to_row.js
@@ -1,7 +1,7 @@
 function showVerticalMessage(string) {
     const MAX_STRING_LENGTH = 10;
-    const FIRST_LETTER = 'м';
-    let row = string.startsWith(FIRST_LETTER) ?
+    const BIG_FIRST_LETTER = 'м';
+    let row = string.startsWith(BIG_FIRST_LETTER) ?
         `\n ${string[0].toUpperCase()}` : `\n ${string[0]}`;
     for (let letter of string.slice(1, MAX_STRING_LENGTH)) {
         row += `\n ${letter}`;
