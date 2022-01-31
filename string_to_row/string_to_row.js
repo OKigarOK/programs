3ba596feb323c66ef8900e4fb81e8816a85d4ce5
Игорь <1730020@gmail.com>
2022-01-31 14:39:12 +0300

    add !

---
+++ string_to_row/string_to_row.js
@@ -0,0 +1,20 @@
+function showVerticalMessage(string) {
+    const maxStringLength = 10;
+    const firstLetter = 'м';
+    let row = '';
+    if (string.length > maxStringLength) {
+        string = string.slice(0, maxStringLength)
+    }
+    if (string[0] === firstLetter) {
+        string = string[0].toUpperCase() + string.slice(1);
+    }
+    for (let letter of string) {
+        row += `\n ${letter} \n`;
+        }
+    console.log(row);
+    }
+
+let str = 'марафон';
+showVerticalMessage(str);
+let str2 = 'приветмедвед';
+showVerticalMessage(str2);
